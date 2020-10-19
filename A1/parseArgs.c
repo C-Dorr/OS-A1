773c2869b311c6c2d72da31d41d0b105e6eab25d
C-Dorr <c.dorr.97@gmail.com>
2020-10-19 15:29:26 -0400

    Options file/header placeholders for parsing

--- A1/parseArgs.c
+++ A1/parseArgs.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "options.h"
 
 const int GETOPT_FAILURE = -1;
 
@@ -19,6 +20,12 @@ int main(int argc, char *argv[]) {
     int op;
     int pid;
     bool pid_was_set = false;
+
+
+    char state_char;
+    char* uptime;
+    char* virtual_mem;
+    char* cmd_line;
 
     while ((op = getopt(argc, argv, "p:stvc")) != GETOPT_FAILURE) {
         switch (op) {
@@ -28,21 +35,23 @@ int main(int argc, char *argv[]) {
 		pid_was_set = true;
 	        break;
             case 's':
-		//Single Character State Information
+	        //Single Character State Information
+                state_char = return_state_character(pid);
 		break;
             case 't':
-		//utime information
+	        //utime information
+		uptime = return_uptime(pid);
 	        break;
             case 'v':
 		//Virtual Memory Used (Pages)
+		virtual_mem = return_virtual_mem(pid);
 	        break;
             case 'c':
 		//Display Command Line which started this process
-                break;
-	    default:
-                printf("No recognized arguments\n");
-		exit(EXIT_FAILURE);
-	}	
+                cmd_line = return_command_line(pid);
+		break;
+
+        }
     }
     if (pid_was_set) {
         printf("PID: %i\n", pid);
